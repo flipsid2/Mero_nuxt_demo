a40823a96a88f18c7361d4d46b16f7fa74ada070
R <R@Rui-MacBook-Pro.local>
2021-07-27 22:05:45 +0900

    api test

--- server-middleware/buyma.js
+++ server-middleware/buyma.js
@@ -108,7 +108,7 @@ app.post('/create', cors(corsOptions), async (req, res, next) => {
     const payload = req.body
     const { token } = payload;
 
-    const data = {
+    const param = {
         "product": {
             "reference_number": "ABC123",
             "control": "publish",
@@ -238,7 +238,7 @@ app.post('/create', cors(corsOptions), async (req, res, next) => {
     const url = default_url + '/api/v1/products.json';
 
     try {
-        const { data } = await axios.post(url, data, config);
+        const { data } = await axios.post(url, param, config);
         if (data.Error) {
             res.status(400)
             .json(data.Error)
@@ -289,7 +289,7 @@ app.post('/orders', cors(corsOptions), async (req, res, next) => {
     console.log('Buyma /orders post : ', token);
 
     //GET /api/v1/orders.json?page=2&per_page=1
-    const url = default_url + '/api/v1/orders.json?page=1&per_page=1';
+    const url = default_url + '/api/v1/orders.json';
     try {
         const { data } = await axios.get(url, config);
         if (data.Error) {
